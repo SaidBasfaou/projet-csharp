8e5c1f3116842044e6247a91e493a592a7db5b84
Said Basfaou <77440794+SaidBasfaou@users.noreply.github.com>
2021-02-11 20:30:29 +0100

    Add files via upload

---
+++ mini_projet/mini_projet/Class1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mini_projet
+{
+    class Produit
+    {
+        private int numP;
+        private string nomP;
+        private double prix;
+        private int Qnt;
+
+        public static int nb = 0;
+
+        public Produit()
+        { nb++; }
+        public Produit(int a,string b,double c,int d)
+        {
+            this.numP = a;
+            this.nomP = b;
+            this.prix = c;
+            if (d>=0)
+            {
+                this.Qnt = d;
+            }
+            else
+            {
+                throw new Exception("attention");
+            }
+            nb++;
+        }
+        public Produit(Produit P)
+        {
+            this.numP = P.numP;
+            this.nomP = P.nomP;
+            this.prix = P.prix;
+            this.Qnt = P.Qnt;
+            nb++;
+        }
+        public int _numP
+        {
+            get { return this.numP; }
+            set { this.numP = value; }
+        }
+        public string _nomP
+        {
+            get { return this.nomP; }
+            set { this.nomP = value; }
+        }
+
+        public double _prix
+        {
+            get { return this.prix; }
+            set { this.prix = value; }
+        }
+        public int _Qnt
+        {
+            get { return this.Qnt; }
+            set 
+            { 
+                if(value>=0)
+                { 
+                    this.Qnt = value;
+                }
+                else
+                {
+                    throw new Exception("attention");
+                }
+                     
+            }
+        }
+        public void affichage()
+        {
+            Console.WriteLine("le num " + this.numP + " le nom " + this.nomP + " le prix " + this.prix + " la quantite " + this.Qnt);
+        }
+        public static bool Compare(Produit P1,Produit P2)
+        {
+            bool res = false;
+            if(P1.numP==P2.numP)
+            {
+                res = true;
+            }
+            return res;
+        }
+        public void prix_aug(double a)
+        {
+            this.prix = this.prix + (this.prix = (a / 100));
+        }
+    }
+}
---
+++ mini_projet/mini_projet/Class2.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mini_projet
+{
+    class Magazin
+    {
+        private int id;
+        private string address;
+        List<Produit> liste_produits = new List<Produit>();
+        public Magazin()
+        {}
+        public Magazin(int a,string b,List<Produit>l)
+        {
+            this.id = a;
+            this.address = b;
+            this.liste_produits = l;
+        }
+        public Magazin(Magazin M)
+        {
+            this.id = M.id;
+            this.address = M.address;
+            this.liste_produits = M.liste_produits;
+        }
+
+        public int _id
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+        public string _address
+        {
+            get { return this.address; }
+            set { this.address = value; }
+        }
+        public void ajouter(Produit P)
+        {
+            this.liste_produits.Add(P);
+        }
+        public void affichage_liste()
+        {
+            foreach (Produit p in this.liste_produits)
+            {
+                p.affichage();
+            }
+        }
+        public int Recherche(int num)
+        {
+            int pos = -1;
+            for (int i = 0; i < this.liste_produits.Count; i++)
+            {
+                if(this.liste_produits[i]._numP==num)
+                {
+                    pos = i;
+                }
+            }
+            return pos;
+        }
+        public void supprimer(int num)
+        {
+            int x = Recherche(num);
+            if(x==-1)
+            {
+                Console.WriteLine("le num n'existe pas");
+            }
+            else
+            {
+                this.liste_produits.RemoveAt(x);
+            }
+        }
+        public void modifier(int num,Produit p)
+        {
+            int y = Recherche(num);
+            if(y==-1)
+            {
+                Console.WriteLine("le num n'existe pas");
+            }
+            else
+            {
+                this.liste_produits[y] = p;
+            }
+        }
+        public void Repture_en_stock()
+        {
+            for (int i = 0; i < this.liste_produits.Count; i++)
+            {
+                if(liste_produits[i]._Qnt==0)
+                {
+                    liste_produits[i].affichage();
+                }
+            }
+        }
+
+    }
+}
